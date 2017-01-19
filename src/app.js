840d870576579c098285d7c3087d2267fe17133d
Dave Harms <daveharmswebdev@gmail.com>
2017-01-18 18:10:35 -0600

    added firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDBGsD79n3V1KNSvqSYMhK_cFt0m-dKdCs',
+      authDomain: 'personalvitalsigns.firebaseapp.com',
+      databaseURL: 'https://personalvitalsigns.firebaseio.com',
+      storageBucket: 'personalvitalsigns.appspot.com',
+      messagingSenderId: '447978014897'
+    });
+  }
+
   render() {
     return (
       <View>
